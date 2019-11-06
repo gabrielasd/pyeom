9df54e3ccdf4ffaf563cf77b03e599fd91bb4768
Gabriela <gsdiaz21@gmail.com>
2019-11-06 14:56:56 -0500

    Clean ionization methods tests

--- eomee/test/test_ionization.py
+++ eomee/test/test_ionization.py
@@ -9,300 +9,255 @@ import numpy as np
 from scipy.linalg import eig, svd
 
 
-def test_ionization_h2_sto6g_symmetrized():
-    """
-    H2 sto6g
-    
+def check_inputs_symm(oneint, twoint, onedm, twodm):
+    """Check symmetry of electron integrals and Density Matrices."""
+    # Electron integrals and DMs symmetric permutations
+    assert np.allclose(oneint, oneint.T)
+    assert np.allclose(onedm, onedm.T)
+    assert np.allclose(twoint, np.einsum('pqrs->rspq', twoint))
+    assert np.allclose(twoint, np.einsum('pqrs->qpsr', twoint))
+    assert np.allclose(twodm, np.einsum('pqrs->rspq', twodm))
+    assert np.allclose(twodm, np.einsum('pqrs->qpsr', twodm))
+    # Two-electron integrals  and 2DM antisymmetric permutations
+    assert np.allclose(twoint, -np.einsum('pqrs->pqsr', twoint))
+    assert np.allclose(twoint, -np.einsum('pqrs->qprs', twoint))
+    assert np.allclose(twodm, -np.einsum('pqrs->pqsr', twodm))
+    assert np.allclose(twodm, -np.einsum('pqrs->qprs', twodm))
+
+
+def test_ionizationeomstate_h2_sto6g():
+    """Test IonizationEOMState for H2 (STO-6G)
+    against Hartree-Fock canonical orbital energy and
+    experimental results.
+
+    HF MO_i: -0.58205888
+    Experiment: 15.42593 eV
+
     """
     one_mo = np.load(find_datafiles('test/h2_sto6g_oneint_genzd.npy'))
     two_mo = np.load(find_datafiles('test/h2_sto6g_twoint_genzd_anti.npy'))
     one_dm = np.load(find_datafiles('test/1dm_h2_sto6g_genzd.npy'))
     two_dm = np.load(find_datafiles('test/2dm_h2_sto6g_genzd_anti.npy'))
-
-    # One-electron integral symmetric permutations
-    assert np.allclose(one_mo, one_mo.conj().T)
-    # Two-electron integrals symmetric permutations
-    assert np.allclose(two_mo, two_mo.conj().transpose((2,3,0,1)))
-    assert np.allclose(two_mo, two_mo.transpose((3,2,1,0)))
-    assert np.allclose(two_mo, two_mo.transpose((1,0,3,2)))
-    # 1DM symmetric permutations
-    assert np.allclose(one_dm, one_dm.conj().T)
-    # 2DM symmetric permutations
-    assert np.allclose(two_dm, two_dm.transpose((1,0,3,2)))
-    assert np.allclose(two_dm, two_dm.transpose((3,2,1,0)))
-    assert np.allclose(two_dm, two_dm.transpose((2,3,0,1)))
-    # 2DM antisymmetric permutations
-    assert np.allclose(two_dm, -two_dm.transpose((0,1,3,2)))
-    assert np.allclose(two_dm, -two_dm.transpose((1,0,2,3)))
-    assert np.allclose(two_dm, -two_dm.transpose((2,3,1,0)))
-    assert np.allclose(two_dm, -two_dm.transpose((3,2,0,1)))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
 
     eom = eomee.IonizationEOMState(one_mo, two_mo, one_dm, two_dm)
     aval, avec = eom.solve_dense()
-    print(aval)
-    # Reference value from
-    # HORTON RHF
+    # Reference value from HORTON RHF
     # horton_emo = [-0.58205888, 0.66587228]
     ie = 0.58205888
-    print(ie)
     assert abs(aval[0] - ie) < 1e-8
     assert abs(aval[2] - ie) < 1e-8
-    # Reference value from
-    # https://webbook.nist.gov/chemistry/
-    # IE = 15.42593 eV
+    # Reference value from https://webbook.nist.gov/chemistry/
+    # IP = 15.42593 eV
     ie = 0.566892206
     assert abs(aval[0] - ie) < 0.1
     assert abs(aval[2] - ie) < 0.1
 
 
-def test_ionization_heh_sto3g_symmetrized():
-    """
-    HeH+ sto3g
-    
+def test_ionizationeomstate_heh_sto3g():
+    """Test IonizationEOMState for HeH+ (STO-3G)
+    against Hartree-Fock canonical orbital energy.
+
+    HF MO_i: -1.52378328
+
     """
     one_mo = np.load(find_datafiles('test/heh+_sto3g_oneint_genzd.npy'))
     two_mo = np.load(find_datafiles('test/heh+_sto3g_twoint_genzd_anti.npy'))
     one_dm = np.load(find_datafiles('test/1dm_heh+_sto3g_genzd.npy'))
     two_dm = np.load(find_datafiles('test/2dm_heh+_sto3g_genzd_anti.npy'))
-
-    # One-electron integral symmetric permutations
-    assert np.allclose(one_mo, one_mo.conj().T)
-    # Two-electron integrals symmetric permutations
-    assert np.allclose(two_mo, two_mo.conj().transpose((2,3,0,1)))
-    assert np.allclose(two_mo, two_mo.transpose((3,2,1,0)))
-    assert np.allclose(two_mo, two_mo.transpose((1,0,3,2)))
-    # 1DM symmetric permutations
-    assert np.allclose(one_dm, one_dm.conj().T)
-    # 2DM symmetric permutations
-    assert np.allclose(two_dm, two_dm.transpose((1,0,3,2)))
-    assert np.allclose(two_dm, two_dm.transpose((3,2,1,0)))
-    assert np.allclose(two_dm, two_dm.transpose((2,3,0,1)))
-    # 2DM antisymmetric permutations
-    assert np.allclose(two_dm, -two_dm.transpose((0,1,3,2)))
-    assert np.allclose(two_dm, -two_dm.transpose((1,0,2,3)))
-    assert np.allclose(two_dm, -two_dm.transpose((2,3,1,0)))
-    assert np.allclose(two_dm, -two_dm.transpose((3,2,0,1)))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
 
     eom = eomee.IonizationEOMState(one_mo, two_mo, one_dm, two_dm)
     aval, avec = eom.solve_dense()
-    print(aval)
-    # Reference value from
-    # HORTON RHF
+    # Reference value from HORTON RHF
     # horton_emo = [-1.52378328, -0.26764028]
     ie = 1.52378328
     assert abs(aval[0] - ie) < 1e-6
 
 
-def test_ionization_he_ccpvdz_symmetrized():
-    """
-    He ccpvdz antisymmetrized
-    
+def test_ionizationeomstate_he_ccpvdz():
+    """Test IonizationEOMState for He (cc-pVDZ)
+    against Hartree-Fock canonical orbital energy and
+    experimental results.
+
+    HF MO_i: -0.91414765
+    Experiment: 24.58738880 eV
+
     """
     one_mo = np.load(find_datafiles('test/he_ccpvdz_oneint_genzd.npy'))
     two_mo = np.load(find_datafiles('test/he_ccpvdz_twoint_genzd_anti.npy'))
     one_dm = np.load(find_datafiles('test/1dm_he_ccpvdz_genzd.npy'))
     two_dm = np.load(find_datafiles('test/2dm_he_ccpvdz_genzd_anti.npy'))
-
-    # One-electron integral symmetric permutations
-    assert np.allclose(one_mo, one_mo.conj().T)
-    # Two-electron integrals symmetric permutations
-    assert np.allclose(two_mo, two_mo.conj().transpose((2,3,0,1)))
-    assert np.allclose(two_mo, two_mo.transpose((3,2,1,0)))
-    assert np.allclose(two_mo, two_mo.transpose((1,0,3,2)))
-    # 1DM symmetric permutations
-    assert np.allclose(one_dm, one_dm.conj().T)
-    # 2DM symmetric permutations
-    assert np.allclose(two_dm, two_dm.transpose((1,0,3,2)))
-    assert np.allclose(two_dm, two_dm.transpose((3,2,1,0)))
-    assert np.allclose(two_dm, two_dm.transpose((2,3,0,1)))
-    assert np.allclose(two_dm, -two_dm.transpose((0,1,3,2)))
-    assert np.allclose(two_dm, -two_dm.transpose((1,0,2,3)))
-    assert np.allclose(two_dm, -two_dm.transpose((2,3,1,0)))
-    assert np.allclose(two_dm, -two_dm.transpose((3,2,0,1)))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
 
     eom = eomee.IonizationEOMState(one_mo, two_mo, one_dm, two_dm)
     aval, avec = eom.solve_dense()
-    print(aval)
-    # Reference value from
-    # HORTON RHF
+    # Reference value from HORTON RHF
     ie = 0.91414765
     assert abs(aval[0] - ie) < 1e-6
-    # Reference value from
-    # https://webbook.nist.gov/chemistry/
-    # IE = 24.58738880 eV
+    # Reference value from https://webbook.nist.gov/chemistry/
+    # IP = 24.58738880 eV
     ie = 0.90356944896
     assert abs(aval[0] - ie) < 0.1
 
 
-def test_ionization_ne_sto6g_symmetrized():
-    """
-    Ne sto-6g antisymmetrized
-    
-    """
-    one_mo = np.load(find_datafiles('test/ne_sto6g_oneint_genzd.npy'))
-    two_mo = np.load(find_datafiles('test/ne_sto6g_twoint_genzd_anti.npy'))
-    one_dm = np.load(find_datafiles('test/1dm_ne_sto6g_genzd.npy'))
-    two_dm = np.load(find_datafiles('test/2dm_ne_sto6g_genzd_anti.npy'))
+def test_ionizationeomstate_ne_321g():
+    """Test IonizationEOMState for Ne (3-21G)
+    against Hartree-Fock canonical orbital energy and
+    experimental results.
 
-    # One-electron integral symmetric permutations
-    assert np.allclose(one_mo, one_mo.conj().T)
-    # Two-electron integrals symmetric permutations
-    assert np.allclose(two_mo, two_mo.conj().transpose((2,3,0,1)))
-    assert np.allclose(two_mo, two_mo.transpose((3,2,1,0)))
-    assert np.allclose(two_mo, two_mo.transpose((1,0,3,2)))
-    # 1DM symmetric permutations
-    assert np.allclose(one_dm, one_dm.conj().T)
-    # 2DM symmetric permutations
-    assert np.allclose(two_dm, two_dm.transpose((1,0,3,2)))
-    assert np.allclose(two_dm, two_dm.transpose((3,2,1,0)))
-    assert np.allclose(two_dm, two_dm.transpose((2,3,0,1)))
-    # 2DM antisymmetric permutations
-    assert np.allclose(two_dm, -two_dm.transpose((0,1,3,2)))
-    assert np.allclose(two_dm, -two_dm.transpose((1,0,2,3)))
-    assert np.allclose(two_dm, -two_dm.transpose((2,3,1,0)))
-    assert np.allclose(two_dm, -two_dm.transpose((3,2,0,1)))
+    HF MO_i: -0.79034293
+    Experiment: 21.564540 eV
 
-    eom = eomee.IonizationEOMState(one_mo, two_mo, one_dm, two_dm)
-    aval, avec = eom.solve_dense()
-    print(aval)
-    # Reference value from
-    # HORTON RHF
-    ie = 0.5607611
-    print(ie)
-    assert abs(aval[-1] - ie) < 1e-8
-    # Reference value from
-    # https://webbook.nist.gov/chemistry/
-    # IE =  21.564540 eV
-    ie = 0.79248185659
-    # THIS ASSERT FAILS
-    # assert abs(aval[-1] - ie) < 0.1
-
-
-def test_ionization_ne_321g_symmetrized():
-    """
-    Ne 3-21g
-    
     """
     one_mo = np.load(find_datafiles('test/ne_321g_oneint_genzd.npy'))
     two_mo = np.load(find_datafiles('test/ne_321g_twoint_genzd_anti.npy'))
     one_dm = np.load(find_datafiles('test/1dm_ne_321g_genzd.npy'))
     two_dm = np.load(find_datafiles('test/2dm_ne_321g_genzd_anti.npy'))
-
-    # One-electron integral symmetric permutations
-    assert np.allclose(one_mo, one_mo.conj().T)
-    # Two-electron integrals symmetric permutations
-    assert np.allclose(two_mo, two_mo.conj().transpose((2,3,0,1)))
-    assert np.allclose(two_mo, two_mo.transpose((3,2,1,0)))
-    assert np.allclose(two_mo, two_mo.transpose((1,0,3,2)))
-    # 1DM symmetric permutations
-    assert np.allclose(one_dm, one_dm.conj().T)
-    # 2DM symmetric permutations
-    assert np.allclose(two_dm, two_dm.transpose((1,0,3,2)))
-    assert np.allclose(two_dm, two_dm.transpose((3,2,1,0)))
-    assert np.allclose(two_dm, two_dm.transpose((2,3,0,1)))
-    assert np.allclose(two_dm, -two_dm.transpose((0,1,3,2)))
-    assert np.allclose(two_dm, -two_dm.transpose((1,0,2,3)))
-    assert np.allclose(two_dm, -two_dm.transpose((2,3,1,0)))
-    assert np.allclose(two_dm, -two_dm.transpose((3,2,0,1)))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
 
     eom = eomee.IonizationEOMState(one_mo, two_mo, one_dm, two_dm)
     aval, avec = eom.solve_dense()
-    print(aval)
-    # Reference value from
-    # HORTON RHF
-    # horton_emo = np.asarray([-32.56471038, -1.8651519, -0.79034293, -0.79034293, -0.79034293, 2.68726251, 2.68726251, 2.68726251, 4.08280903])
+    # Reference value from HORTON RHF
+    # horton_emo = np.asarray([-32.56471038, -1.8651519, -0.79034293, -0.79034293,
+    #                        -0.79034293, 2.68726251, 2.68726251, 2.68726251, 4.08280903])
     ie = 0.79034293
-    print(ie)
-    print(aval[4])
     assert abs(aval[4] - ie) < 1e-5
-    # assert np.allclose(-aval, horton_mos)
-    # Reference value from
-    # https://webbook.nist.gov/chemistry/
-    # IE =  21.564540 eV
+    # Reference value from https://webbook.nist.gov/chemistry/
+    # IP =  21.564540 eV
     ie = 0.79248185659
     assert abs(aval[4] - ie) < 0.01
 
 
-def test_ionization_be_sto3g_symmetrized():
-    """
-    Be sto-3g antisymmetrized
+def test_ionizationeomstate_be_sto3g():
+    """Test IonizationEOMState for Be (STO-3G)
+    against Hartree-Fock canonical orbital energy and
+    experimental results.
+
+    HF MO_i: -0.25403769
+    Experiment: 9.322699 eV
 
     """
     one_mo = np.load(find_datafiles('test/be_sto3g_oneint_genzd.npy'))
     two_mo = np.load(find_datafiles('test/be_sto3g_twoint_genzd_anti.npy'))
     one_dm = np.load(find_datafiles('test/1dm_be_sto3g_genzd.npy'))
     two_dm = np.load(find_datafiles('test/2dm_be_sto3g_genzd_anti.npy'))
-
-    # One-electron integral symmetric permutations
-    assert np.allclose(one_mo, one_mo.conj().T)
-    # Two-electron integrals symmetric permutations
-    assert np.allclose(two_mo, two_mo.conj().transpose((2,3,0,1)))
-    assert np.allclose(two_mo, two_mo.transpose((3,2,1,0)))
-    assert np.allclose(two_mo, two_mo.transpose((1,0,3,2)))
-    # 1DM symmetric permutations
-    assert np.allclose(one_dm, one_dm.conj().T)
-    # 2DM symmetric permutations
-    assert np.allclose(two_dm, two_dm.transpose((1,0,3,2)))
-    assert np.allclose(two_dm, two_dm.transpose((3,2,1,0)))
-    assert np.allclose(two_dm, two_dm.transpose((2,3,0,1)))
-    assert np.allclose(two_dm, -two_dm.transpose((0,1,3,2)))
-    assert np.allclose(two_dm, -two_dm.transpose((1,0,2,3)))
-    assert np.allclose(two_dm, -two_dm.transpose((2,3,1,0)))
-    assert np.allclose(two_dm, -two_dm.transpose((3,2,0,1)))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
 
     eom = eomee.IonizationEOMState(one_mo, two_mo, one_dm, two_dm)
     aval, avec = eom.solve_dense()
-    print(aval)
-    # Reference value from
-    # HORTON RHF
-    horton_mos = np.asarray([-4.48399211, -0.25403769, 0.22108596, 0.22108596, 0.22108596])
+    # Reference value from HORTON RHF
+    horton_mos = np.asarray([-4.48399211, -0.25403769, 0.22108596,
+                             0.22108596, 0.22108596])
     ie = 0.25403769
-    print(aval[1])
     assert abs(aval[1] - ie) < 1e-8
     assert np.allclose(-aval[:2], horton_mos[:2])
-    # Reference value from
-    # https://webbook.nist.gov/chemistry/
-    # IE =  9.322699 eV
+    # Reference value from https://webbook.nist.gov/chemistry/
+    # IP =  9.322699 eV
     ie = 0.3426027085
     assert abs(aval[1] - ie) < 0.1
 
 
-def test_ionization_b_sto3g_symmetrized():
-    """
-    B sto-3g
-    
+def test_ionizationeomstate_b_sto3g():
+    """Test IonizationEOMState for B (STO-3G)
+    against Hartree-Fock canonical orbital energy.
+
+    HF MO_i: -0.20051823
+
     """
     one_mo = np.load(find_datafiles('test/1mo_b_sto3g_genzd.npy'))
     two_mo = np.load(find_datafiles('test/2mo_b_sto3g_genzd_anti.npy'))
     one_dm = np.load(find_datafiles('test/1dm_b_sto3g_genzd.npy'))
     two_dm = np.load(find_datafiles('test/2dm_b_sto3g_genzd_anti.npy'))
-
-    # One-electron integral symmetric permutations
-    assert np.allclose(one_mo, one_mo.conj().T)
-    # Two-electron integrals symmetric permutations
-    assert np.allclose(two_mo, two_mo.conj().transpose((2,3,0,1)))
-    assert np.allclose(two_mo, two_mo.transpose((3,2,1,0)))
-    assert np.allclose(two_mo, two_mo.transpose((1,0,3,2)))
-    # 1DM symmetric permutations
-    assert np.allclose(one_dm, one_dm.conj().T)
-    # 2DM symmetric permutations
-    assert np.allclose(two_dm, two_dm.transpose((1,0,3,2)))
-    assert np.allclose(two_dm, two_dm.transpose((3,2,1,0)))
-    assert np.allclose(two_dm, two_dm.transpose((2,3,0,1)))
-    assert np.allclose(two_dm, -two_dm.transpose((0,1,3,2)))
-    assert np.allclose(two_dm, -two_dm.transpose((1,0,2,3)))
-    assert np.allclose(two_dm, -two_dm.transpose((2,3,1,0)))
-    assert np.allclose(two_dm, -two_dm.transpose((3,2,0,1)))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
 
     eom = eomee.IonizationEOMState(one_mo, two_mo, one_dm, two_dm)
     aval, avec = eom.solve_dense()
-    print(aval)
     # HORTON UHF alpha HOMO
     ie = 0.20051823
     assert abs(aval[-3] - ie) < 1e-8
-    # Reference value from
-    # https://webbook.nist.gov/chemistry/
-    # IE =  9.322699 eV
-    ie = 0.3426027085
+    # Reference value from https://webbook.nist.gov/chemistry/
+    # IP =  8.29803 eV
+    ie = 0.30494683
+    # This assert fails
+    # assert abs(aval[-3] - ie) < 0.1
+
+
+def test_ionizationdoublecommutator_he_ccpvdz():
+    """Test IonizationDoubleCommutator for He (cc-pVDZ)
+    against Hartree-Fock canonical orbital energy and
+    experimental results.
+
+    HF MO_i: -0.91414765
+    Experiment: 24.58738880 eV
+
+    """
+    one_mo = np.load(find_datafiles('test/he_ccpvdz_oneint_genzd.npy'))
+    two_mo = np.load(find_datafiles('test/he_ccpvdz_twoint_genzd_anti.npy'))
+    one_dm = np.load(find_datafiles('test/1dm_he_ccpvdz_genzd.npy'))
+    two_dm = np.load(find_datafiles('test/2dm_he_ccpvdz_genzd_anti.npy'))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
+
+    eom = eomee.IonizationDoubleCommutator(one_mo, two_mo, one_dm, two_dm)
+    aval, avec = eom.solve_dense()
+    # Reference value from HORTON RHF
+    ie = 0.91414765
+    assert abs(aval[0] - ie) < 1e-6
+    # Reference value from https://webbook.nist.gov/chemistry/
+    # IP = 24.58738880 eV
+    ie = 0.90356944896
+    assert abs(aval[0] - ie) < 0.1
+
+
+def test_ionizationdoublecommutator_ne_321g():
+    """Test IonizationDoubleCommutator for Ne (3-21G)
+    against Hartree-Fock canonical orbital energy and
+    experimental results.
+
+    HF MO_i: -0.79034293
+    Experiment: 21.564540 eV
+
+    """
+    one_mo = np.load(find_datafiles('test/ne_321g_oneint_genzd.npy'))
+    two_mo = np.load(find_datafiles('test/ne_321g_twoint_genzd_anti.npy'))
+    one_dm = np.load(find_datafiles('test/1dm_ne_321g_genzd.npy'))
+    two_dm = np.load(find_datafiles('test/2dm_ne_321g_genzd_anti.npy'))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
+
+    eom = eomee.IonizationDoubleCommutator(one_mo, two_mo, one_dm, two_dm)
+    aval, avec = eom.solve_dense()
+    # Reference value from HORTON RHF
+    # horton_emo = np.asarray([-32.56471038, -1.8651519, -0.79034293, -0.79034293,
+    #                        -0.79034293, 2.68726251, 2.68726251, 2.68726251, 4.08280903])
+    ie = 0.79034293
+    assert abs(aval[4] - ie) < 1e-5
+    # Reference value from https://webbook.nist.gov/chemistry/
+    # IP =  21.564540 eV
+    ie = 0.79248185659
+    assert abs(aval[4] - ie) < 0.01
+
+
+def test_ionizationdoublecommutator_b_sto3g():
+    """Test IonizationDoubleCommutator for B (STO-3G)
+    against Hartree-Fock canonical orbital energy.
+
+    HF MO_i: -0.20051823
+    Experiment: 8.29803 eV
+
+    """
+    one_mo = np.load(find_datafiles('test/1mo_b_sto3g_genzd.npy'))
+    two_mo = np.load(find_datafiles('test/2mo_b_sto3g_genzd_anti.npy'))
+    one_dm = np.load(find_datafiles('test/1dm_b_sto3g_genzd.npy'))
+    two_dm = np.load(find_datafiles('test/2dm_b_sto3g_genzd_anti.npy'))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
+
+    eom = eomee.IonizationDoubleCommutator(one_mo, two_mo, one_dm, two_dm)
+    aval, avec = eom.solve_dense()
+    # HORTON UHF alpha HOMO
+    ie = 0.20051823
+    assert abs(aval[-3] - ie) < 1e-8
+    # Reference value from https://webbook.nist.gov/chemistry/
+    # IP =  8.29803 eV
+    ie = 0.30494683
     # This assert fails
     # assert abs(aval[-3] - ie) < 0.1
