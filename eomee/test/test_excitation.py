15d5111fa453bf2a079bfcfc75a7ed97a9f7bf66
Michael Richer <richerm@mcmaster.ca>
2019-12-17 17:18:34 -0500

    add excitation ERPA (WIP)

--- eomee/test/test_excitation.py
+++ eomee/test/test_excitation.py
@@ -45,3 +45,32 @@ def test_excitationeom_heh_sto3g():
     # E_S1 = 24.7959 eV = 0.91123209 Hartree
     e = 0.91123209
     assert abs(aval[-1] - e) < 1e-6
+
+def test_excitationeom_erpa_heh_sto3g():
+    """Test Excitation ERPA for HeH+ (STO-3G)
+
+    """
+    one_mo = np.load(find_datafiles('test/heh+_sto3g_oneint_genzd.npy'))
+    two_mo = np.load(find_datafiles('test/heh+_sto3g_twoint_genzd_anti.npy'))
+    one_dm = np.load(find_datafiles('test/1dm_heh+_sto3g_genzd.npy'))
+    two_dm = np.load(find_datafiles('test/2dm_heh+_sto3g_genzd_anti.npy'))
+    check_inputs_symm(one_mo, two_mo, one_dm, two_dm)
+
+    n = one_mo.shape[0]
+    aa = one_mo[:1, :1]
+    bb = one_mo[n//2:(n//2 + 1), n//2:(n//2 + 1)]
+    aaaa = two_mo[:1, :1, :1, :1]
+    abab = two_mo[:1, n//2:(n//2 + 1), :1, n//2:(n//2 + 1)]
+    baba = two_mo[n//2:(n//2 + 1), :1, n//2:(n//2 + 1), :1]
+    bbbb = two_mo[n//2:(n//2 + 1), n//2:(n//2 + 1), n//2:(n//2 + 1), n//2:(n//2 + 1)]
+    one_mo_0 = np.zeros_like(one_mo)
+    two_mo_0 = np.zeros_like(two_mo)
+    one_mo_0[:1, :1] = aa
+    one_mo_0[n//2:(n//2 + 1), n//2:(n//2 + 1)] = bb
+    two_mo_0[:1, :1, :1, :1] = aaaa
+    two_mo_0[:1, n//2:(n//2 + 1), :1, n//2:(n//2 + 1)] = abab
+    two_mo_0[n//2:(n//2 + 1), :1, n//2:(n//2 + 1), :1] = baba
+    two_mo_0[n//2:(n//2 + 1), n//2:(n//2 + 1), n//2:(n//2 + 1), n//2:(n//2 + 1)] = bbbb
+
+    ecorr = eomee.ExcitationEOM.erpa(one_mo_0, two_mo_0, one_mo, two_mo, one_dm, two_dm)
+    print(ecorr)
