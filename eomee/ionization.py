928055e51bbbd7730156e5bad8260ab7b41096c6
Gabriela Sanchez-Diaz <gsdiaz21@gmail.com>
2023-10-05 13:44:56 -0400

    Add EOMIP flavors with killer condition just on left-hand-side.

--- eomee/ionization.py
+++ eomee/ionization.py
@@ -132,6 +132,58 @@ class EOMIPDoubleCommutator(EOMState):
         return m
 
 
+class EOMIPDoubleCommutator0(EOMState):
+    r"""
+    Ionization EOM state for operator :math:`\hat{Q}_k = \sum_n { c_n a_n }`.
+
+    .. math::
+
+        \left< \Psi^{(N)}_0 \middle| \left[a^{\dagger}_m, \left[ \hat{H}, \hat{Q} \right] \right] \middle| \Psi^{(N)}_0 \right>
+        = \Delta_k \left< \Psi^{(N)}_0 \middle| a^{\dagger}_m \hat{Q} \middle| \Psi^{(N)}_0 \right>
+
+    """
+
+    @property
+    def neigs(self):
+        r"""
+        Return the size of the eigensystem.
+
+        Returns
+        -------
+        neigs : int
+            Size of eigensystem.
+
+        """
+        # Number of q_n terms = n_{\text{basis}}
+        return self._n
+
+    def _compute_lhs(self):
+        r"""
+        Compute
+
+        .. math::
+
+            A_{mn} = h_{nm} -2 \sum_q { h_{nq} \gamma_{mq} } + \sum_{qs} { \left< nq||ms \right> \gamma_{qs} }
+            + \sum_{qrs} { \left< nq||rs \right> \Gamma_{mqsr} }.
+
+        """
+        # A_mn = h_mn - 2 * \gamma_mq * h_qn
+        a = np.copy(self._h)
+        a -= 2 * np.dot(self._dm1, self._h)
+        # A_mn += <v_msnq> \gamma_sq - \Gamma_mqrs <v_nqrs>
+        a += np.einsum("msnq,sq", self._v, self._dm1, optimize=True)
+        a -= np.einsum("mqrs,nqrs", self._dm2, self._v, optimize=True)
+        return a
+
+    def _compute_rhs(self):
+        r"""
+        Compute :math:`M_{mn} = \gamma_{mn}`.
+
+        """
+        # M_mn = \gamma_mn
+        return self._dm1
+
+
 class EOMIPAntiCommutator(EOMState):
     r"""
     Ionization EOM state for operator :math:`\hat{Q}_k = \sum_n { c_n a_n }`.
@@ -176,3 +228,48 @@ class EOMIPAntiCommutator(EOMState):
         # M_mn = \delta_mn
         m = np.eye(self._n, dtype=self._dm1.dtype)
         return m
+
+
+class EOMIPAntiCommutator0(EOMState):
+    r"""
+    Ionization EOM state for operator :math:`\hat{Q}_k = \sum_n { c_n a_n }`.
+
+    .. math::
+
+        \left< \Psi^{(N)}_0 \middle| \Big\{ a^{\dagger}_m, \left[ \hat{H}, \hat{Q} \right] \Big\} \middle| \Psi^{(N)}_0 \right>
+        = \Delta_k \left< \Psi^{(N)}_0 \middle| a^{\dagger}_m \hat{Q} \middle| \Psi^{(N)}_0 \right>
+
+    """
+
+    @property
+    def neigs(self):
+        r"""
+        Return the size of the eigensystem.
+
+        Returns
+        -------
+        neigs : int
+            Size of eigensystem.
+
+        """
+        # Number of q_n terms = n_{\text{basis}}
+        return self._n
+
+    def _compute_lhs(self):
+        r"""
+        Compute :math:`A_{mn} = -h_{nm} + \sum_{qr} { \left< qn||mr \right> \gamma_{qr} }`.
+
+        """
+        # A_mn = -h_mn
+        a = -np.copy(self._h)
+        # A_mn += <v_qnmr> \gamma_qr
+        a += np.einsum("qnmr,qr", self._v, self._dm1, optimize=True)
+        return a
+
+    def _compute_rhs(self):
+        r"""
+        Compute :math:`M_{mn} = \gamma_{mn}`.
+
+        """
+        # M_mn = \gamma_mn
+        return self._dm1
