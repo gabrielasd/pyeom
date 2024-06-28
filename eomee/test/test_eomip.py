0c7a5caa3ea8b4e63abd2762d62471b739bf8523
Gabriela Sanchez-Diaz <gsdiaz21@gmail.com>
2024-06-28 15:06:41 -0400

    Fix minor typos.

--- eomee/test/test_eomip.py
+++ eomee/test/test_eomip.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with EOMEE. If not, see <http://www.gnu.org/licenses/>.
 
-r"""Test eomee.ionization."""
+r"""Test eomee.eomip."""
 
 
 # import eomee
