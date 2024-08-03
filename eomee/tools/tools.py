e3bf328849c06d483f005fa50f0cfd1558934ba0
Gabriela Sanchez-Diaz <gsdiaz21@gmail.com>
2024-08-03 09:43:03 -0400

    Import utility functions to the eomee.tools namespace.

--- eomee/tools/tools.py
+++ eomee/tools/tools.py
@@ -25,6 +25,10 @@ __all__ = [
     "antisymmetrize",
     "from_unrestricted",
     "hartreefock_rdms",
+    "spinize_rdms",
+    "make_spinized_fock_hamiltonian",
+    "make_doci_ham_spinized",
+    "make_gvbpp_hamiltonian",
 ]
 
 
